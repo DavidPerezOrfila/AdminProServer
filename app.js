50095d89868d9d5b5e07eb26eaaa5f7dbdd2b6c3
David Pérez <knightbcn@gmail.com>
2019-03-22 06:59:26 +0100

    fix CORS

--- app.js
+++ app.js
@@ -25,9 +25,12 @@ mongoose.connection.openUri(process.env.URLDB, { useNewUrlParser: true }, (err, 
 app.use(express.json());
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
+
+// CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET,  PUT, DELETE, OPTIONS");
     next();
 });
 
